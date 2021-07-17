ded02101be84d5a239d6bcd9cdb5a135b7b97256
Justin Seitz <justinseitz2016@gmail.com>
2021-07-17 14:02:47 -0700

    added db

--- index.js
+++ index.js
@@ -8,20 +8,22 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api'
 app.get('/api/tasks', (req, res) => {
-  // Return them as json
-  res.json(
-    {tasks: [
-        {'id': '1', 'title': 'task1', 'details': 'task1 details'},
-        {'id': '2', 'title': 'task2', 'details': 'task2 details'}
-    ]});
+    // Return them as json
+    res.json(
+        {
+            tasks: [
+                { 'id': '1', 'title': 'task1', 'details': 'task1 details' },
+                { 'id': '2', 'title': 'task2', 'details': 'task2 details' }
+            ]
+        });
 
-  console.log(`Sent tasks`);
+    console.log(`Sent tasks`);
 });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
@@ -29,3 +31,25 @@ app.listen(port);
 
 console.log(`Listening on ${port}`);
 
+// Database stuff
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+app.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM test_table');
+        const results = { 'results': (result) ? result.rows : null };
+        res.render('pages/db', results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
+})
+// End database stuff
