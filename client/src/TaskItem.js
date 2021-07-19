172479b95af96b1ba5bb02657ac6fc91070e258b
Justin Seitz <justinseitz2016@gmail.com>
2021-07-19 11:55:30 -0700

    edit button toggles showing edit fields

--- client/src/TaskItem.js
+++ client/src/TaskItem.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export function TaskItem(props) {
+    const [showEdit, setShowEdit] = useState(false);
+
     const handleClick = () => {
         props.deleteTask(props.id);
     }
@@ -10,6 +12,12 @@ export function TaskItem(props) {
             <h2>{props.title}</h2>
             <p>{props.details}</p>
             <button onClick={handleClick}>Delete</button>
+            <button onClick={()=>setShowEdit(showEdit===true ? false : true)}>Edit</button>
+            {showEdit && (
+            <div>
+                <input type="text" placeholder="Title" />
+                <input type="text" placeholder="Details" />
+            </div>)}
         </div>
     );
 }
